40e6b626eadab8c19563c09e8af7377b8a9efdf7
einav242 <93386470+einav242@users.noreply.github.com>
2022-03-22 21:29:50 +0200

    Add files via upload

---
+++ Notebook.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include "Direction.hpp"
+#include "Notebook.hpp"
+// using ariel::Direction;
+
+namespace ariel{
+        Notebook::Notebook()
+        {
+            
+            // line=0;
+            // colum=0;
+            // page=0;
+        }
+        void Notebook::write(int page,int row,int colum,Direction d, const string & data)
+        {
+            
+        }
+        string Notebook::read(int page,int row,int colum,Direction d,int data)
+        {
+            return " ";
+        }
+        void Notebook::erase(int page,int row,int colum,Direction d,int data)
+        {
+            
+        }
+        void Notebook::show(int page)
+        {
+            
+        }
+};
+
+
+
